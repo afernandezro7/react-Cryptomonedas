cab176961ac101850cc5ffa46efb62ec955444fb
Alberto Fernández <a.fernandez.ro7@gmail.com>
2020-09-25 03:19:38 -0500

    rename build folder to docs

---
+++ docs/precache-manifest.5c98d7aa6097219642eeec12a1bcee9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7339c2cad28d67fb35d1df619d56dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d335b0dcebbb918f464",
+    "url": "/static/css/main.08b449d4.chunk.css"
+  },
+  {
+    "revision": "82d2cf4a37a8bb2507e0",
+    "url": "/static/js/2.450ff502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.450ff502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d335b0dcebbb918f464",
+    "url": "/static/js/main.1dc7a1ab.chunk.js"
+  },
+  {
+    "revision": "27343cb691de655e28f8",
+    "url": "/static/js/runtime-main.ba78b093.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
